e463752a340e2dca13a7c1cfb909082ce898e4a7
Joshua Arnott <joshua.arnott@gigaclear.com>
2018-10-31 16:41:11 +0000

    Version bump to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import RustExtension, Binding
 
 setup(
     name="PyValico",
-    version="0.0.1",
+    version="0.0.2",
     packages=["valico"],
     rust_extensions=[RustExtension("valico.valico", binding=Binding.RustCPython)],
     setup_requires=["setuptools_rust"],
